c29954a0e35c80f402dc00d307f1cb263d9dbb74
vyedid@frba.utn.edu.ar <vyedid@frba.utn.edu.ar>
2024-04-02 19:07:30 -0300

    Add files via upload

---
+++ src/test/java/ar/edu/utn/frba/dds/InscripcionTest.java
@@ -0,0 +1,37 @@
+import domain.Alumno;
+import domain.Inscripcion;
+import domain.Materia;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+public class InscripcionTest {
+
+    Materia discreta = new Materia("Matematica Discreta",List.of());
+    Materia algoritmos = new Materia("Algoritmos de Programacion",List.of());
+    Materia paradigmas = new Materia("Paradigmas de Programacion",List.of(algoritmos, discreta));
+
+    @Test
+
+    public void ElAlumnoSePuedeInscribirALasMaterias(){
+        Alumno florencia = new Alumno("Florencia",List.of(discreta,algoritmos));
+        Inscripcion inscripcionFlorencia = new Inscripcion(List.of(paradigmas),florencia);
+
+        Assertions.assertTrue(inscripcionFlorencia.aprobada());
+
+    }
+    @Test
+    public void ElAlumnoNoSePuedeInscribirALasMaterias(){
+
+        Alumno carlos = new Alumno("Carlos",List.of(algoritmos));
+        Inscripcion inscripcionCarlos = new Inscripcion(List.of(paradigmas),carlos);
+
+        Assertions.assertFalse(inscripcionCarlos.aprobada());
+
+    }
+
+
+
+}
